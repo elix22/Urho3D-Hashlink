1a0c67932d46cffbfbaefcbef6016e5b6ed28ad6
Eli Aloni <elialoni@Elis-MacBook-Pro.local>
2020-08-16 23:30:36 +0300

    move Rotator.hx to utils folder

---
+++ src/haxe/utils/Rotator.hx
@@ -0,0 +1,37 @@
+package utils;
+import urho3d.*;
+
+class Rotator extends LogicComponent {
+	private var rotationSpeed:Vector3;
+
+	var counter = 0;
+
+	public static function CreateFactory()
+	{
+		var args = new Array<Dynamic>();
+		return Type.createInstance(Rotator,args);
+	}
+
+	public function new() {
+		super(this);
+	}
+
+	public function SetRotationSpeed(speed:Vector3) {
+		rotationSpeed = speed;
+	}
+
+	public override function Start() {
+	}
+
+	public override function DelayedStart() {
+	}
+
+	public override function Update(timeStep:Float) {
+		node.Rotate(new TQuaternion(rotationSpeed.x * timeStep, rotationSpeed.y * timeStep, rotationSpeed.z * timeStep));
+	}
+
+
+	public function ResetRotation() {
+		node.rotation = new Quaternion(Random(360.0), Random(360.0), Random(360.0));
+	}
+}
